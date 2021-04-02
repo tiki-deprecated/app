a62adf3d4e9c87b15cdbcf6aee49a684ef374c37
Mike Audi <mike@mytiki.com>
2021-04-02 03:00:03 -0400

    trying manual provisioning

---
+++ ios/ci/install-certificates.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Copyright (c) TIKI Inc.
+# MIT license. See LICENSE file in root directory.
+#
+
+# (h/t) https://medium.com/@karaiskc/archive-and-export-ios-app-with-github-actions-b44f676e4bf9
+
+security create-keychain -p "" build.keychain
+
+security import "ios/ci/development.cer" -k "$KEYCHAIN" -P "" -A
+security import "ios/ci/distribution.cer" -k "$KEYCHAIN" -P "" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" "$KEYCHAIN"
---
+++ ios/ci/install-provisions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# Copyright (c) TIKI Inc.
+# MIT license. See LICENSE file in root directory.
+#
+
+# (h/t) https://medium.com/@karaiskc/archive-and-export-ios-app-with-github-actions-b44f676e4bf9
+
+mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
+
+for PROVISION in `ls ./*.mobileprovision`
+do
+  UUID=`/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i ./$PROVISION)`
+  cp "./$PROVISION" "$HOME/Library/MobileDevice/Provisioning Profiles/$UUID.mobileprovision"
+done
