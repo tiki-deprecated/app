92b2c3b97d7aff2190f5e51f462f9614daa94071
Mike Audi <mike@mytiki.com>
2021-04-02 23:28:53 -0400

    move keychain to local dir
    online hints suggest if you put it anywhere but the keychain dir it wont prompt the user for "login" during signing

--- .github/scripts/install-certificates.sh
+++ .github/scripts/install-certificates.sh
@@ -7,16 +7,16 @@
 
 # (h/t) https://medium.com/@karaiskc/archive-and-export-ios-app-with-github-actions-b44f676e4bf9
 
-security create-keychain -p "" ~/Library/Keychains/build.keychain
+security create-keychain -p "" ios/build.keychain
 
-security import "ios/AppleWWDRCAG3.cer" -k ~/Library/Keychains/build.keychain -P "" -A
-security import "ios/Development.p12" -t agg -k ~/Library/Keychains/build.keychain -P "" -A
-security import "ios/Distribution.p12" -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security import "ios/AppleWWDRCAG3.cer" -k ios/build.keychain -P "" -A
+security import "ios/Development.p12" -t agg -k ios/build.keychain -P "" -A
+security import "ios/Distribution.p12" -t agg -k ios/build.keychain -P "" -A
 
-security list-keychains -s ~/Library/Keychains/build.keychain
-security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security list-keychains -s ios/build.keychain
+security default-keychain -s ios/build.keychain
+security unlock-keychain -p "" ios/build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ios/build.keychain
 
 security find-identity -v -p codesigning
--- .github/scripts/install-provisions.sh
+++ .github/scripts/install-provisions.sh
@@ -7,10 +7,10 @@
 
 # (h/t) https://medium.com/@karaiskc/archive-and-export-ios-app-with-github-actions-b44f676e4bf9
 
-mkdir -p "$HOME/Library/MobileDevice/Provisioning Profiles"
+mkdir -p "~/Library/MobileDevice/Provisioning Profiles"
 
-for PROVISION in `ls ./ios/*.mobileprovision`
+for PROVISION in `ls ios/*.mobileprovision`
 do
   UUID=`/usr/libexec/PlistBuddy -c 'Print :UUID' /dev/stdin <<< $(security cms -D -i ./$PROVISION)`
-  cp "./$PROVISION" "$HOME/Library/MobileDevice/Provisioning Profiles/$UUID.mobileprovision"
+  cp "./$PROVISION" "~/Library/MobileDevice/Provisioning Profiles/$UUID.mobileprovision"
 done
