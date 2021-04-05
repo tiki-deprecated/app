2daa7086ff2f2d7c9a99ffb4c5fc8fc00075c9c4
Mike Audi <mike@mytiki.com>
2021-04-04 23:25:57 -0400

    add sentry to flutter
    Fixes #30

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:app/src/configs/config_sentry.dart' as ConfigSentry;
+import 'package:app/src/utilities/utility_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(App());
+  await SentryFlutter.init(
+      (options) async => options
+        ..dsn = ConfigSentry.of(ConfigSentry.dsn)
+        ..environment = ConfigSentry.of(ConfigSentry.environment)
+        ..release = await version()
+        ..sendDefaultPii = false,
+      appRunner: () => runApp(App()));
 }
