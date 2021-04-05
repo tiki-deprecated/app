ccd5f3a9617abd1cc288a0e3fe9df9d7991a0dad
Mike Audi <mike@mytiki.com>
2021-04-05 08:44:34 -0400

    dsn of '' for local instead of null

--- lib/src/configs/config_sentry.dart
+++ lib/src/configs/config_sentry.dart
@@ -9,7 +9,7 @@ const String dsn = 'dsn';
 const String environment = 'environment';
 
 T of<T>(String key) {
-  return appEnv == 'public' ? _publicConfig[key] : null;
+  return (appEnv == 'public' ? _publicConfig : _localConfig)[key];
 }
 
 const Map<String, Object> _publicConfig = {
@@ -17,3 +17,5 @@ const Map<String, Object> _publicConfig = {
       'https://8074e92e5fc04829b519f700e3295fd2@o564671.ingest.sentry.io/5705532',
   environment: appEnv
 };
+
+const Map<String, Object> _localConfig = {dsn: '', environment: appEnv};
