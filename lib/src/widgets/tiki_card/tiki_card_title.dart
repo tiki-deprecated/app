f512ab2a556fe6521f95b6e8739da367236b4c39
Ricardo Gonçalves <ricardo@brgweb.com.br>
2021-05-22 07:05:49 -0300

    merge homeCards

--- lib/src/widgets/tiki_card/tiki_card_title.dart
+++ lib/src/widgets/tiki_card/tiki_card_title.dart
@@ -2,14 +2,14 @@ import 'package:app/src/utils/platform/platform_relative_size.dart';
 import 'package:flutter/material.dart';
 
 class TikiCardTitle extends StatelessWidget {
-  final String title;
-  final Color textColor;
+  final String? title;
+  final Color? textColor;
 
-  const TikiCardTitle({Key key, this.title, this.textColor});
+  const TikiCardTitle(this.title, this.textColor);
 
   @override
   Widget build(BuildContext context) {
-    return Text(title,
+    return Text(title ?? "",
     style: TextStyle(
       color: textColor ?? Colors.black,
       fontSize: 22,
