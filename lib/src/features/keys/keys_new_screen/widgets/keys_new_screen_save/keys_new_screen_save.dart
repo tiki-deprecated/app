469f498a505d847f8920aa1a4d8b51c6c4cba9f5
Ricardo Gonçalves <ricardo@brgweb.com.br>
2021-05-25 01:23:25 -0300

    fix save screen layout

--- lib/src/features/keys/keys_new_screen/widgets/keys_new_screen_save/keys_new_screen_save.dart
+++ lib/src/features/keys/keys_new_screen/widgets/keys_new_screen_save/keys_new_screen_save.dart
@@ -21,38 +21,56 @@ class KeysNewScreenSave extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<KeysNewScreenBloc, KeysNewScreenState>(
         builder: (BuildContext context, KeysNewScreenState state) {
-      return Column(children: [
-        Container(
-            margin: EdgeInsets.symmetric(
-                vertical: 2 * PlatformRelativeSize.blockVertical),
-            width: 10 * PlatformRelativeSize.blockVertical,
-            height: 10 * PlatformRelativeSize.blockVertical,
-            decoration: BoxDecoration(
-                border: Border.fromBorderSide(
-                    BorderSide(width: 2, color: Colors.red)),
-                borderRadius: BorderRadius.all(
-                    Radius.circular(12 * PlatformRelativeSize.blockVertical))),
-            child: Center(
-                child: Text("!",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Koara",
-                        color: Colors.red,
-                        fontSize: 6 * PlatformRelativeSize.blockVertical)))),
-        Center(child: TikiTitle("Backup\nyour account")),
-        Container(
-            margin: EdgeInsets.symmetric(
-                vertical: 2 * PlatformRelativeSize.blockVertical),
-            child: TikiSubtitle(
-              "We recommend you to securely save your key in case you change your device.",
-              fontSize: 2 * PlatformRelativeSize.blockVertical,
-              fontWeight: FontWeight.normal,
-            )),
-        KeysNewScreenSaveBk(),
-        KeysNewScreenSaveBkDownload(),
-        KeysNewScreenSaveContinue(),
-        KeysNewScreenSaveRestore()
-      ]);
+      return Container(
+          height: MediaQuery.of(context).size.height * 0.95,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        child: Column(
+                      children: [
+                        Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical:
+                                    2 * PlatformRelativeSize.blockVertical),
+                            width: 10 * PlatformRelativeSize.blockVertical,
+                            height: 10 * PlatformRelativeSize.blockVertical,
+                            decoration: BoxDecoration(
+                                border: Border.fromBorderSide(
+                                    BorderSide(width: 2, color: Colors.red)),
+                                borderRadius: BorderRadius.all(Radius.circular(
+                                    12 * PlatformRelativeSize.blockVertical))),
+                            child: Center(
+                                child: Text("!",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: "Koara",
+                                        color: Colors.red,
+                                        fontSize: 6 *
+                                            PlatformRelativeSize
+                                                .blockVertical)))),
+                        Center(child: TikiTitle("Backup\nyour account")),
+                        Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical:
+                                    2 * PlatformRelativeSize.blockVertical),
+                            child: TikiSubtitle(
+                              "We recommend you to securely save your key in case you change your device.",
+                              fontSize: 2 * PlatformRelativeSize.blockVertical,
+                              fontWeight: FontWeight.normal,
+                            )),
+                        KeysNewScreenSaveBk(),
+                        KeysNewScreenSaveBkDownload(),
+                        KeysNewScreenSaveContinue(),
+                      ],
+                    ))
+                  ],
+                ),
+                KeysNewScreenSaveRestore()
+              ]));
     });
   }
 }
