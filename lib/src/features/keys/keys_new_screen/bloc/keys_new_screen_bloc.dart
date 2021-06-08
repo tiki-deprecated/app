47d2f0868188b0830cfc6b28dd5d18545829b430
Ricardo Gonçalves <ricardo@brgweb.com.br>
2021-06-08 04:57:18 -0300

    fix encrypt merge

--- lib/src/features/keys/keys_new_screen/bloc/keys_new_screen_bloc.dart
+++ lib/src/features/keys/keys_new_screen/bloc/keys_new_screen_bloc.dart
@@ -15,8 +15,10 @@ import 'package:app/src/features/repo/repo_local_ss_keys/repo_local_ss_keys.dart
 import 'package:app/src/features/repo/repo_local_ss_keys/repo_local_ss_keys_model.dart';
 import 'package:app/src/features/repo/repo_local_ss_user/repo_local_ss_user.dart';
 import 'package:app/src/features/repo/repo_local_ss_user/repo_local_ss_user_model.dart';
+import 'package:app/src/utils/helper/crypto/helper_crypto_ecdsa.dart';
+import 'package:app/src/utils/helper/crypto/helper_crypto_rsa.dart';
 import 'package:app/src/utils/helper/helper_api_rsp.dart';
-import 'package:app/src/utils/helper/helper_crypto.dart';
+import 'package:app/src/utils/helper/crypto/helper_crypto.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
@@ -68,14 +70,14 @@ class KeysNewScreenBloc extends Bloc<KeysNewScreenEvent, KeysNewScreenState> {
   Stream<KeysNewScreenState> _mapGeneratedToState(
       KeysNewScreenGenerated generated) async* {
     AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> dataKey =
-        await HelperCrypto.createRSA();
+        await HelperCryptoRsa.createRSA();
     AsymmetricKeyPair<ECPublicKey, ECPrivateKey> signKey =
-        await HelperCrypto.createECDSA();
+        await HelperCryptoEcdsa.createECDSA();
 
-    String dataPublic = HelperCrypto.encodeRSAPublic(dataKey.publicKey);
-    String dataPrivate = HelperCrypto.encodeRSAPrivate(dataKey.privateKey);
-    String signPublic = HelperCrypto.encodeECDSAPublic(signKey.publicKey);
-    String signPrivate = HelperCrypto.encodeECDSAPrivate(signKey.privateKey);
+    String dataPublic = HelperCryptoRsa.encodeRSAPublic(dataKey.publicKey);
+    String dataPrivate = HelperCryptoRsa.encodeRSAPrivate(dataKey.privateKey);
+    String signPublic = HelperCryptoEcdsa.encodeECDSAPublic(signKey.publicKey);
+    String signPrivate = HelperCryptoEcdsa.encodeECDSAPrivate(signKey.privateKey);
     String address = HelperCrypto.sha3(signPublic);
     yield KeysNewScreenInProgress(
         dataPublic, dataPrivate, signPublic, signPrivate, address, false);
