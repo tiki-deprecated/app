db358f26e41d3e67687717c24dd17912b9c9c86e
Ricardo Gonçalves <ricardo@brgweb.com.br>
2021-07-02 17:20:44 -0300

    login screen sizes

--- lib/src/slices/login_screen/ui/login_screen_layout_background_email.dart
+++ lib/src/slices/login_screen/ui/login_screen_layout_background_email.dart
@@ -16,11 +16,11 @@ class LoginScreenBackgroundEmail extends StatelessWidget {
       Container(
           margin: EdgeInsets.only(top: 2.h),
           alignment: Alignment.topRight,
-          child: HelperImage('login-pineapple')),
+          child: HelperImage('login-pineapple', height: 30.h)),
       Container(
           margin: EdgeInsets.only(top: 32.h),
           alignment: Alignment.topLeft,
-          child: HelperImage('login-blob'))
+          child: HelperImage('login-blob', height: 20.h))
     ]);
   }
 }
