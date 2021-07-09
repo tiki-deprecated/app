78e38915020a60744682bee2987aa548a8dbf9eb
Ricardo Gonçalves <ricardo@brgweb.com.br>
2021-07-09 20:24:00 -0300

    home navigator icons

---
+++ lib/src/widgets/icons.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:flutter/widgets.dart';
+
+@immutable
+class _TikiIconsData extends IconData {
+  const _TikiIconsData(int codePoint)
+      : super(
+          codePoint,
+          fontFamily: 'TikiIcons',
+        );
+}
+
+@immutable
+class TikiIcons {
+  TikiIcons._();
+
+  static const IconData choices = _TikiIconsData(0xe000);
+  static const IconData eye = _TikiIconsData(0xe001);
+  static const IconData money = _TikiIconsData(0xe002);
+}
