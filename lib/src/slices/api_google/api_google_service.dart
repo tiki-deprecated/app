dc1becb21a1252a74bbee63da33aa6c5c77e08ed
Ricardo Gonçalves <ricardo@brgweb.com.br>
2021-07-17 19:07:34 -0300

    WIP

--- lib/src/slices/api_google/api_google_service.dart
+++ lib/src/slices/api_google/api_google_service.dart
@@ -6,11 +6,13 @@
 import 'package:app/src/slices/info_carousel_card/model/info_carousel_card_model.dart';
 import 'package:app/src/utils/helper_json.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/gmail/v1.dart';
 
 import 'repository/api_google_repository_info.dart';
 
 class ApiGoogleService {
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final GoogleSignIn _googleSignIn =
+      GoogleSignIn(scopes: <String>[GmailApi.gmailModifyScope]);
   final ApiGoogleRepositoryInfo _googleInfoRepository =
       ApiGoogleRepositoryInfo();
 
@@ -36,5 +38,52 @@ class ApiGoogleService {
     List<dynamic>? infoJson = await _googleInfoRepository.load();
     return HelperJson.listFromJson(
         infoJson, (s) => InfoCarouselCardModel.fromJson(s));
+  }
+
+  fetchGmail() async {
+    GmailApi gmailApi;
+    final authHeaders = await googleUser.authHeaders;
+    final authenticateClient = GoogleAuthClient(authHeaders);
+
+    List<gmailApi.Message> messagesList = [];
+    Future waitForInit;
+
+    gmailApi = gMail.GmailApi(authenticateClient);
+
+    gMail.ListMessagesResponse results =
+        await gmailApi.users.messages.list("me");
+    for (gMail.Message message in results.messages) {
+      gMail.Message messageData =
+          await gmailApi.users.messages.get("me", message.id);
+      messagesList.add(messageData);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("email")),
+      body: buildFutureBuilder(),
+    );
+  }
+
+  FutureBuilder buildFutureBuilder() {
+    return FutureBuilder(
+        future: waitForInit,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            List<Widget> messageTxt = [];
+            for (var m in messagesList) {
+              messageTxt.add(Text(m.snippet));
+            }
+            return Column(
+              children: messageTxt,
+            );
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        });
   }
 }
