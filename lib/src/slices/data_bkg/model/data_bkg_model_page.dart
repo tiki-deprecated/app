262da6de997921deb50b907a9e3df9f8c3cacf10
Mike Audi <mike@mytiki.com>
2021-08-06 13:27:58 -0400

    page google api, check email on open

---
+++ lib/src/slices/data_bkg/model/data_bkg_model_page.dart
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+class DataBkgModelPage<T> {
+  List<T>? data;
+  dynamic? next;
+
+  DataBkgModelPage({this.data, this.next});
+}
