582ac634cee0d460d03205cc70c5d02f445ecdce
Ricardo Gonçalves <ricardo@brgweb.com.br>
2021-08-12 09:17:29 -0300

    fix html email

--- lib/src/slices/api_google/api_google_service.dart
+++ lib/src/slices/api_google/api_google_service.dart
@@ -118,17 +118,24 @@ to: $to
 from: me
 subject: $subject
 
-Content-Type: text/plain; charset="UTF-8"
-
-Hello,
-
-I'd like to stop receiving emails from this email list.
-
-Thanks,
-${_googleSignIn.currentUser?.displayName ?? ''}
-
-*Sent via <a href="http://www.mytiki.com>TIKI</a>. Join the data ownership
-revolution today.
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.0 Transitional//EN” “https://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd”>
+<html xmlns=“https://www.w3.org/1999/xhtml”>
+<head>
+<title>Test Email Sample</title>
+<meta http–equiv=“Content-Type” content=“text/html; charset=UTF-8” />
+<meta http–equiv=“X-UA-Compatible” content=“IE=edge” />
+<meta name=“viewport” content=“width=device-width, initial-scale=1.0 “ />
+</head>
+<body class=“em_body” style=“margin:0px; padding:0px;”> 
+Hello,<br /><br />
+I'd like to stop receiving emails from this email list.<br /><br />
+Thanks,<br /><br />
+${_googleSignIn.currentUser?.displayName ?? ''}<br />
+<br />
+*Sent via http://www.mytiki.com. Join the data ownership<br />
+revolution today.<br />
+</body>
+</html>
 ''';
 
     await gmailApi.users.messages
