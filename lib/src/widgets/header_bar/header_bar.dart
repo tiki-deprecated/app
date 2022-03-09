99b25c822fe0642b24a1a516ab88e05046d9847f
Ricardo Gonçalves <ricardo@mytiki.com>
2022-03-09 16:41:32 -0300

    add account sdk fix decision sdk dependencies add linter cleanup files

--- lib/src/widgets/header_bar/header_bar.dart
+++ lib/src/widgets/header_bar/header_bar.dart
@@ -3,36 +3,23 @@
  * MIT license. See LICENSE file in root directory.
  */
 import 'package:flutter/material.dart';
-import 'package:login/login.dart';
-import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
+import 'package:user_account/user_account.dart';
 
 import '../../config/config_size.dart';
-import '../../slices/api_app_data/api_app_data_service.dart';
-import '../../slices/api_short_code/api_short_code_service.dart';
-import '../../slices/api_signup/api_signup_service.dart';
-import '../../slices/user_account_modal/user_account_modal_service.dart';
-import '../../slices/user_referral/user_referral_service.dart';
 import 'header_bar_view_badge.dart';
 
 class HeaderBar extends StatelessWidget {
+  final UserAccount userAccount;
+
+  const HeaderBar({Key? key, required this.userAccount}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    ApiAppDataService apiAppDataService =
-        Provider.of<ApiAppDataService>(context);
-    ApiSignupService apiSignupService = Provider.of<ApiSignupService>(context);
-    Login login = Provider.of<Login>(context, listen: false);
-    UserReferralService userReferralService = UserReferralService(
-        apiAppDataService,
-        apiSignupService,
-        login,
-        Provider.of<ApiShortCodeService>(context, listen: false));
 
     // TODO fix bottom sheet modal service rebuilds in every tap
     return GestureDetector(
-        onTap: () => UserAccountModalService(userReferralService, login)
-            .presenter
-            .showModal(context),
+        onTap: () => userAccount.open(context),
         behavior: HitTestBehavior.opaque,
         child: Container(
           padding: EdgeInsets.only(
