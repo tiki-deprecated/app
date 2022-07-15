266c126d6e5adb66aaa74f8c8de3d2512f177009
Ricardo Gonçalves <ricardo@mytiki.com>
2022-07-14 21:20:39 -0300

    check for updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
-##0.1.2
-* Full architecture refactor.
+## 0.4.4
+
+* reindex all inboxes
+* check for updates
+* update tiki_data to 0.0.17
--- lib/provide.dart
+++ lib/provide.dart
@@ -8,6 +8,7 @@ import 'dart:convert';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:httpp/httpp.dart';
 import 'package:logging/logging.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -25,6 +26,8 @@ import 'package:tiki_wallet/tiki_wallet.dart';
 import 'src/config/config_amplitude.dart';
 import 'src/home/home_model_overlay.dart';
 import 'src/home/home_service.dart';
+
+Logger log = Logger('provider');
 
 Future<List<SingleChildWidget>> init(HomeService homeService,
     {FlutterSecureStorage? secureStorage,
@@ -128,3 +131,41 @@ Future<void> _truncateLastPageAndLastRunRepos(Database database) async {
       await prefs.setString('reindexed_inbox_version', '0.4.4');
   }
 }
+
+Future<void> checkVersion() async {
+  Uri uri = Uri.parse('https://api.github.com/repos/tiki/app/releases');
+  await Httpp().client().request(HttppRequest(
+      uri: uri,
+      verb: HttppVerb.GET,
+      timeout: const Duration(seconds: 30),
+      onSuccess: (rsp) async {
+      try {
+        List releases = rsp.body?.jsonBody;
+        Map ver = releases[0];
+        String releaseName = ver['name'].trim();
+        String currentName = (await PackageInfo.fromPlatform()).version;
+        if (releaseName != currentName) {
+          SharedPreferences preferences = await SharedPreferences.getInstance();
+          List<String> latest = releaseName.split('.');
+          List<String> current = currentName.split('.');
+          int major1 = int.parse(latest[0]);
+          int major2 = int.parse(current[0]);
+          int minor1 = int.parse(latest[1]);
+          int minor2 = int.parse(current[1]);
+          int hot1 = int.parse(latest[2]);
+          int hot2 = int.parse(current[2]);
+          if (major1 > major2 ||
+              (major1 == major2 && minor1 > minor2) ||
+              (minor1 == minor2 && hot1 > hot2)
+          ) {
+            preferences.setString('update_to', releaseName);
+          } else {
+            preferences.setString('update_to', '');
+          }
+        }
+      }catch(e){
+        log.warning("Error with version check", e);
+      }
+      }));
+}
+
